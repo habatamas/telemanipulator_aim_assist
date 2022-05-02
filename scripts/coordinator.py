8864ff2ed94237a5e2baf15c9e9332fd8250a003
Haba Tamás <habatamas@edu.bme.hu>
2022-05-02 21:18:35 +0200

    Code cleanup.

--- scripts/coordinator.py
+++ scripts/coordinator.py
@@ -2,32 +2,44 @@
 import rospy
 from open_manipulator_msgs.srv import SetKinematicsPose, SetKinematicsPoseRequest
 
-# initialize ros node
-rospy.init_node('telemanipulator_coordinator')
+class Coordinator:
+    def __init__(self):
+        # initialize ros node
+        rospy.init_node('telemanipulator_coordinator')
+        print("node initialized")
 
-# connect to openmanipulator controller service
-rospy.wait_for_service("/open_manipulator/goal_task_space_path_position_only")
+        # initialize calls to robot pose request service
+        rospy.wait_for_service("/open_manipulator/goal_task_space_path_position_only")
+        self.goal_task_space_service = rospy.ServiceProxy("/open_manipulator/goal_task_space_path_position_only", SetKinematicsPose)
 
-service = rospy.ServiceProxy("/open_manipulator/goal_task_space_path_position_only", SetKinematicsPose)
+    def request_pose(self, x, y, z, time=2.0):
+        # create request and set data
+        request = SetKinematicsPoseRequest()
+        request.end_effector_name = "gripper"
+        request.path_time = time
+        request.kinematics_pose.pose.position.x = x
+        request.kinematics_pose.pose.position.y = y
+        request.kinematics_pose.pose.position.z = z
 
+        # call service
+        response = self.goal_task_space_service(request)
+        print(response)
 
-while True:
-    # go to home position
-    request = SetKinematicsPoseRequest()
-    request.end_effector_name = "gripper"
-    request.path_time = 2.0
-    request.kinematics_pose.pose.position.x = 0.134
-    request.kinematics_pose.pose.position.y = 0
-    request.kinematics_pose.pose.position.z = 0.241
-    response = service(request)
-    rospy.sleep(3)
+    def request_home_pose(self):
+        self.request_pose(0.134,0,0.241)
 
-    # go to init position
-    request = SetKinematicsPoseRequest()
-    request.end_effector_name = "gripper"
-    request.path_time = 2.0
-    request.kinematics_pose.pose.position.x = 0.286
-    request.kinematics_pose.pose.position.y = 0
-    request.kinematics_pose.pose.position.z = 0.204
-    response = service(request)
-    rospy.sleep(3)
+    def request_init_pose(self):
+        self.request_pose(0.286,0,0.204)
+
+    def run(self):
+        while True:
+            print("home")
+            self.request_home_pose()
+            rospy.sleep(3)
+            print("init")
+            self.request_init_pose()
+            rospy.sleep(3)
+
+if __name__=="__main__":
+    coordinator_node = Coordinator()
+    coordinator_node.run()
